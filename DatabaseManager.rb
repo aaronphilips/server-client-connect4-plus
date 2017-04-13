1505c44942480fa597848d1830b047b36999407e
aphilips@ualberta.ca <aphilips@ualberta.ca>
2017-04-13 16:36:39 -0600

    push to Reems comp cause no sqlite gem

--- DatabaseManager.rb
+++ DatabaseManager.rb
@@ -17,7 +17,9 @@ class DatabaseManager
 							id INT,
 							clientID INT,
 							ip TEXT,
-							online INT
+							port INT,
+							available INT
+
 						)"
 			@db.execute "CREATE TABLE users
 						(
@@ -34,7 +36,52 @@ class DatabaseManager
 			puts "Exception occurred"
 			puts e
 			@db.rollback
+		ensure
+			@db.close
 		end
+	end
+
+	def add_server(ip,port,clientID)
+		begin
+			@db = SQLite3::Database.open "test.db"
+			@db.transaction
+			stm=@db.prepare "INSERT INTO server(id,clientID,ip,port,available)
+			SELECT IFNULL(MAX(id), 0) + 1,?,?,?,1 from users"
+
+			stm.bind_param 1, clientID
+			stm.bind_param 2, ip
+			stm.bind_param 3, port
+
+			stm.execute
+			# stm.close
+			@db.commit
+			@db.close()
+		rescue SQLite3::Exception => e
+			puts "Exception occurred"
+			puts e
+		end
+	end
+
+	def get_connected_ips
+		begin
+			@db = SQLite3::Database.open "test.db"
+			@db.transaction
+			@db.prepare 'select ip from server where available=1'
+			ip_info = stm.execute
+			ip_info do |row|
+				puts row[0]
+			end
+				
+
+		rescue SQLite3::Exception => e
+			puts "Exception occurred"
+			puts e
+			@db.rollback
+		ensure
+			@db.close
+		end
+
+
 	end
 
 	def get_user(user)
@@ -95,4 +142,8 @@ class DatabaseManager
 
 end
 
+dbm = DatabaseManager.new 
+dbm.set_up
+dbm.add_server(1,2,3)
+dbm.get_connected_ips
 # sm = ServerManager.new
--- MainServer.rb
+++ MainServer.rb
@@ -1,17 +1,29 @@
 require "xmlrpc/server"
 require_relative "CalvinsServer"
 require "socket"
-server = TCPServer.open(2626)
-Queue.new.
-loop do
-	Thread.fork(server.accept) do |client| 
+Queue.new
 
+class MainServer
 
-		client.puts("Hello, I'm Ruby TCP server", "I'm disconnecting, bye :*")
-		client.funny_function
-		client.close
+	def initialize
+
+		@dm = DatabaseManager.new
+		server = TCPServer.open(2626)
+
 	end
+
+	def run
+		@dm.getGameServers
+.
+		loop do
+			Thread.fork(server.accept) do |client| 
+				client.puts("Hello, I'm Ruby TCP server", "I'm disconnecting, bye :*")
+				client.funny_function
+				client.close
+			end
+		end
+
+	end
+
 end
-
-
 # ms=MainServer.new
